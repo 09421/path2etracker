6279cae103f697ea266f213ae90c52b38887e8a6
09421 <lasse@micorbis.dk>
2025-05-22 13:13:46 +0200

    github pages

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="min-h-screen bg-gray-100 dark:bg-gray-800 p-6">
       <ThemeToggle />
       <div className="max-w-3xl mx-auto space-y-6 dark:bg-gray-800"  >
-        <h1 className="text-3xl font-bold text-center dark:text-white">PF2E Initiative & Health Tracker</h1>
+        <h1 className="text-3xl font-bold text-center text-gray-900 dark:text-white">PF2E Initiative & Health Tracker</h1>
         <AddCombatantForm />
         <CombatantList />
       </div>
